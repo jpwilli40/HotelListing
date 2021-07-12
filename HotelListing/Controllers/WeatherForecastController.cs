7c722c37795d47b9b9d3a59bcc720715f9b8ac19
jdubbromeo <jdubbromeo@live.com>
2021-07-12 16:36:38 -0700

    Add project files.

---
+++ HotelListing/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] summaries = new[]
+        {
+            "Freezing", "Chilly", "Cool", "Warm", "Hot"
+        };
+
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            int num = 40;
+
+            return num;
+        }
+    }
+}
